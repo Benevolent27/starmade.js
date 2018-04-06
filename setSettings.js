c51f666e953090bb9427c2b438af5a1b626479c1
Benevolent27 <nolimit8787@yahoo.com>
2018-04-06 09:08:00 -0400

    Beautified the setup questions a bit.

--- setSettings.js
+++ setSettings.js
@@ -9,11 +9,16 @@ var path = require('path');
 //     return require(module)();
 // }
 
-
 var settingsFile=path.join(__dirname, "/settings.json");
 
-// To uninstall from local dependencies:  npm uninstall -save prompt-sync
+// NPM Reminders
+// To uninstall from local dependencies:  npm uninstall -save [Package Name]
+// To install to local folder: npm install -save [Package Name]
+// To install to local folder and to only use for TESTING ONLY:  npm install -save-dev [Package Name]
+
 function forceRequire(nodeRequire){
+  // This is intended to replace require, where it will download remote packages needed if not already available
+  // However there are bugs.  This will ONLY work with packages that install to the node_modules folder bearing the name of the package.
   try {
     return require(nodeRequire);
   } catch (err) {
@@ -23,7 +28,7 @@ function forceRequire(nodeRequire){
       if (installPromptSync.stdout) { console.log(installPromptSync.stdout.toString()); }
       if (installPromptSync.stderr) { console.log(installPromptSync.stderr.toString()); }
       if (installPromptSync.message) { console.log(installPromptSync.message.toString()); }
-      // var installDeps = child.execSync('npm install');
+      // var installDeps = child.execSync('npm install'); // This might be necessary if dependencies aren't auto-installed.
       console.log("'" + nodeRequire + "' module installed successfully!");
     } catch (error) {
       console.error("ERROR:  Could not install the dependency, '" + nodeRequire + "'!  Error code: " + error.status);
@@ -31,14 +36,15 @@ function forceRequire(nodeRequire){
       process.exit(error.status);
     }
   }
-  // Get the main js file from the package.js file..
+  // Get the main js file from the package.js file.  This only works if the package uses it's own name as it's install folder.  For example, the "is-valid-path" package DOES NOT, so it fails for that.
   let thisModuleFolder=path.join(__dirname, "node_modules", nodeRequire);
   let theMainJSFile=path.join(thisModuleFolder, require(thisModuleFolder + "package.json")["main"]);
-  // console.log("Setting require to: " + theMainJSFile);
   return require(theMainJSFile);
 }
 
 function forceRequireWithSettings(nodeRequire,settings){
+// I had to create this function variation because of the package "prompt-sync", which forces a require('prompt-sync')() <-- note the extra parenthesis
+// Every other package that I've been looking at gets fucked up if you try to require it with the extra ()
 let theSettings="";
 if (settings) { theSettings=settings; }
   try {
@@ -50,7 +56,7 @@ if (settings) { theSettings=settings; }
       if (installPromptSync.stdout) { console.log(installPromptSync.stdout.toString()); }
       if (installPromptSync.stderr) { console.log(installPromptSync.stderr.toString()); }
       if (installPromptSync.message) { console.log(installPromptSync.message.toString()); }
-      // var installDeps = child.execSync('npm install');
+      // var installDeps = child.execSync('npm install'); // This might be necessary if dependencies aren't auto-installed.
       console.log("'" + nodeRequire + "' module installed successfully!");
     } catch (error) {
       console.error("ERROR:  Could not install the dependency, '" + nodeRequire + "'!  Error code: " + error.status);
@@ -58,22 +64,18 @@ if (settings) { theSettings=settings; }
       process.exit(error.status);
     }
   }
-
-  // Get the main js file from the package.js file..
+  // Get the main js file from the package.js file.  This only works if the package uses it's own name as it's install folder.  For example, the "is-valid-path" package DOES NOT, so it fails for that.
   let thisModuleFolder=path.join(__dirname, "node_modules", nodeRequire);
   let theMainJSFile=path.join(thisModuleFolder, require(thisModuleFolder + "package.json")["main"]);
-
-  // let thisModuleFolder=__dirname + "/node_modules/" + nodeRequire + "/";
-  // let theMainJSFile=thisModuleFolder + require(thisModuleFolder + "package.json")["main"];
-
-  // console.log("Setting require to: " + theMainJSFile + "(" + theSettings.toString() + ")");
   return require(theMainJSFile)(theSettings);
 }
-const prompt = forceRequireWithSettings("prompt-sync",{"sigint":true}); // https://www.npmjs.com/package/prompt-sync - This creates sync prompts and can have auto-complete capabilties.
+const prompt = forceRequireWithSettings("prompt-sync",{"sigint":true}); // https://www.npmjs.com/package/prompt-sync - This creates sync prompts and can have auto-complete capabilties.  The sigint true part makes it so pressing CTRL + C sends the normal SIGINT to the parent javascript process
 // const decache = forceRequire("decache"); // https://www.npmjs.com/package/decache - This is used to reload requires, such as reloading a json file or mod without having to restart the scripting.
 const isInvalidPath = forceRequire("is-invalid-path"); // https://www.npmjs.com/package/is-invalid-path -- Not using the "is-valid-path" because my force require scripting won't work with it since it uses a non-standard path to it's scripts
 const mkdirp = forceRequire("mkdirp"); // https://www.npmjs.com/package/mkdirp - Great for sync or async folder creation, creating all folders necessary up to the end folder
 
+
+// These are just examples you can uncomment out for uses of "prompt"
 // var testPrompt;
 // testPrompt = prompt("What color is your hair? ") ; console.log("I LOOOVE " + testPrompt + " hair!");
 // testPrompt=prompt("What are you wearing? "); console.log("I wish I was wearing " + testPrompt + "..");
@@ -85,7 +87,7 @@ const mkdirp = forceRequire("mkdirp"); // https://www.npmjs.com/package/mkdirp -
 // while (isNaN(testPrompt=parseInt(prompt("ANOTHER NUMBER! ")))){ }
 // console.log("Final Number: " + testPrompt);
 
-// do testPrompt=parseInt(prompt("DO/WHILE Loop number Please! "));
+// do testPrompt=parseInt(prompt("Gimme a number for the DO/WHILE Loop Please! "));
 // while (isNaN(testPrompt));
 // console.log("Do/While Loop Number: " + testPrompt);
 
@@ -104,26 +106,33 @@ function isRamValue(testVal) {
 
 function portValueTest (num){
   if (num) {
-    if (isNaN(parseInt(num))) { return false; }
+    if (isNaN(parseInt(num))) {
+      console.log("ERROR: '" + num + "' is not a number!");
+      return false;
+    }
+    if (num > 65535 || num < 0) {
+      console.log("ERROR: Port numbers MUST always be between 1 and 65535!  Try again!");
+      return false;
+    }
     return num;
   }
   return 4242;
 }
 
-function testStarMadeDirValue (path) {
-  if (!path) { return path.join(__dirname, "starmade"); }
-  if (!isInvalidPath(path)) { // If the path provided was valid
-    if (fs.existsSync(path)){ // If the path exists, check to see if it is a file or named pipe.  IF so, we cannot use it.
-      if (fs.statSync(path).isFile()) {
-        console.log("ERROR: '" + path + "' already exists as a filename.  Please choose a different directory path!");
-      } else if (fs.statSync(path).isFIFO()) {
-        console.log("ERROR: '" + path + "' already exists as a named pipe.  Please choose a different directory path!");
+function testStarMadeDirValue (installDir) {
+  if (!installDir) { return path.join(__dirname, "starmade"); }
+  if (!isInvalidPath(installDir)) { // If the path provided was valid
+    if (fs.existsSync(installDir)){ // If the path exists, check to see if it is a file or named pipe.  IF so, we cannot use it.
+      if (fs.statSync(installDir).isFile()) {
+        console.log("ERROR: '" + installDir + "' already exists as a filename.  Please choose a different directory path!");
+      } else if (fs.statSync(installDir).isFIFO()) {
+        console.log("ERROR: '" + installDir + "' already exists as a named pipe.  Please choose a different directory path!");
       } else {
-        return path // The path existed and it was not a file or named pipe so we should be able to use it.. unless it's a symlink to a file.. but I figure if someone is using symlinks to a file they should be smart enough to know not to try to use it as their starmade install folder..
+        return installDir // The path existed and it was not a file or named pipe so we should be able to use it.. unless it's a symlink to a file.. but I figure if someone is using symlinks to a file they should be smart enough to know not to try to use it as their starmade install folder..
       }
     }
     // The path specified was valid, but did not exist, so let's just return it.  The next part of the scripting will then create it if it does not exist, making SURE this is the path intended.
-    return path;
+    return installDir;
   }
   // The path was invalid, so throw crap at the person.
   console.log("ERROR: The path you specified is not valid!");
@@ -136,39 +145,41 @@ var settings={};
 var settingSet=false;
 try {
   settings = require(settingsFile);
-  console.log("Imported settings values from " + settingsFile);
+  // console.log("Imported settings values from " + settingsFile);
 } catch (ex) {
-  console.log("'" + settingsFile + "' file not found! Let's set up your install!");
-  settings = {};
+  console.log("'settings.json' did not exist.  Running setup..");
 }
 
 // IF there was a settings.json file imported, ensure that all values are set.
 if (!settings.hasOwnProperty('javaMin')) {
+  console.log("");
   while (!isRamValue(settings["javaMin"]=prompt("Java MIN RAM to use? (Recommended: 512m or higher): "))){ console.log("Please specify a number!  Note: It can end in k, m, or g."); }
   console.log("Min RAM usage set to: " + settings["javaMin"]);
   settingSet=true;
 }
 if (!settings.hasOwnProperty('javaMax')) {
+  console.log("");
   while (!isRamValue(settings["javaMax"]=prompt("Java MAX RAM to use? (Recommended: 4096m or higher): "))){ console.log("Please specify a number!  Note: It can end in k, m, or g."); }
   console.log("Max RAM usage set to: " + settings["javaMax"]);
   settingSet=true;
 }
 if (!settings.hasOwnProperty('port')) {
+  console.log("");
   while (isNaN(parseInt(settings["port"]=portValueTest(prompt("What port shall we use? (Default: 4242): "))))) {
-    console.log("Please specify a number to use as the port!");
+    // console.log("Please specify a number to use as the port!");
   }
   console.log("Port set to: " + settings["port"]);
   settingSet=true;
 }
 if (!settings.hasOwnProperty('starMadeFolder')) {
-  console.log("What StarMade folder should we install to?");
+  console.log("\nWhat StarMade folder should we install to?");
   console.log("If not specified, we'll use the same directory as starmade.js. (recommended)");
   while (!(settings["starMadeFolder"]=testStarMadeDirValue(prompt(": ")))) { /* empty on purpose */ }
   console.log("StarMade Install Folder set to: " + settings["starMadeFolder"]);
   if (!fs.existsSync(settings["starMadeFolder"])){
-    console.log("The StarMade Install folder does not exist!");
-    if (prompt("Press [ENTER] to create the folder.  Anything else will exit this script!")){
-      console.log("Aborting settings change and exiting script!");
+    console.log("\nThe StarMade Install folder does not exist!");
+    if (prompt("Press [ENTER] to confirm creating the folder.")){
+      console.log("Something else was typed!  Abort ABORT!");
       process.exit(130);
     }
     try {
@@ -188,10 +199,12 @@ if (!settings.hasOwnProperty('starMadeFolder')) {
 }
 // Only write to the file IF new settings were set, otherwise leave it alone.
 if (settingSet==true) {
+  console.log("");
   try {
     var settingsFileStream=fs.createWriteStream(settingsFile);
     settingsFileStream.write(JSON.stringify(settings));
     settingsFileStream.end();
+    console.log("Successfully wrote new settings to settings.json file!")
   } catch (err) {
     console.error("ERROR: Could not write to settings.json file!");
     if (err) {
