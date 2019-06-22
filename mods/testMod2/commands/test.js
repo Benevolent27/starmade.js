4a0ebd1efe32bc5501805d45d459e3046e140699
Benevolent27 <nolimit8787@yahoo.com>
2019-06-22 00:57:55 -0400

    Removing mods during testing

--- mods/testMod2/commands/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: 'This command is used to ping the server for a response time!',
-    exclusive: true,
-    priority: 0,
-    execute(playerObj, args) {
-        console.log('Pong.');
-    },
-};
