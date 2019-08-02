9a52b10e5ef5c24385c26d7aae386251fcc60a48
Benevolent27 <nolimit8787@yahoo.com>
2019-08-01 21:16:29 -0400

    switched to promise based commands

--- mods/exampleMods/spawns.js
+++ mods/exampleMods/spawns.js
@@ -1,5 +1,11 @@
 
+// I highly recommend reading this article to understand how to do error handling with promises:
+// https://javascript.info/promise-error-handling
+
+
 global.event.on("playerSpawn", playerSpawn);
 function playerSpawn (player) {
-    player.botMsg("Welcome to the server you gnarly toad!");
+    // These will complete as quickly as possible.
+    player.botMsgPromise("Welcome to the server you bodacious turtle!").catch((err) => console.error(err));
+    player.botMsgPromise("Welcome welcome!",{"type":"info"}).catch((err) => console.error(err));
 };
