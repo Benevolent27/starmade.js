7e0b2d7966d5a3032fdb55467415e46eb3372184
Benevolent27 <nolimit8787@yahoo.com>
2020-01-27 05:46:28 -0500

    Updated to the final InstallObj type

--- mods-ServerDefaults/default/starter.js
+++ mods-ServerDefaults/default/starter.js
@@ -3,9 +3,10 @@
 
 var serverObjJs=require("serverObj.js");
 var {ServerObj}=serverObjJs;
-var serverPath=global.getServerPath(__dirname);
-global.event.on("init",function(){ // Only start the server when the init is given, which is after all initial setup and installs have been done by starmade.js
-    var serverObj=new ServerObj(serverPath); // The ServerObj loads it's own settings, installs itself if needed, and starts the server if autoStart is set to true.
-    global.regServer(serverPath,serverObj); // Registers the server to the global variable.
-    global.event.emit('start',serverPath); // This is when mods should initialize themselves to utilize the serverObj.  This does NOT mean the server is started.
+// var serverPath=global.getServerPath(__dirname);
+var installObj=global.getInstallObj(__dirname);
+installObj.event.on("init",function(){ // Only start the server when the init is given, which is after all initial setup and installs have been done by starmade.js
+    var serverObj=new ServerObj(installObj.path); // The ServerObj loads it's own settings, installs itself if needed, and starts the server if autoStart is set to true.
+    global.regServer(installObj.path,serverObj); // Registers the server to the global installObj.
+    installObj.event.emit('start'); // This is when mods should initialize themselves to utilize the serverObj.  This does NOT mean the server is started.
 });
