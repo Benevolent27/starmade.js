9cbb600233792c6f00f49e87bf3df5e6a2add60d
Benevolent27 <nolimit8787@yahoo.com>
2020-01-30 04:35:29 -0500

    updated to use the installObj.reload()

--- mods-ServerDefaults/default/modReloader.js
+++ mods-ServerDefaults/default/modReloader.js
@@ -1,17 +1,18 @@
 // The purpose of this default mod is to assist with reloading other mods (including itself)
 
 var installObj = global.getInstallObj(__dirname);
-var settings = installObj.settings;
-var event = installObj.event;
 var installPath=installObj.path;
+var {settings,event,reloadMods}=installObj;
 event.on('commandStart',function(serverObj){
     serverObj.regCommand("reloadMods","HiddenHelpers",true,false);
     serverObj.regCommand("listeners","HiddenHelpers",true,false);
     event.on('command', function (player,command,args,messageObj) { // Normally we would not use the messageObj, but it's here if for some reason we want the command to operate differently depending on channel sent to
         if (command == "reloadmods"){
             player.botMsg("Reloading all mods!  This might take a few seconds.");
-            event.emit("reloadMods");
-            // I need to send a global command to remove the event listeners, this will not suffice.
+            reloadMods(); // This will emit a "unloadMods" event
+            // event.emit("unloadMods");
+            // TODO: I need to send a global command to remove the event listeners, this will not suffice.
+            player.botMsg("Done!");
         } else if (command == "listeners"){ // TODO: Change this so it looks at all event listeners and then lists each.
             console.log("command:");
             console.dir(event.listeners('command'));
