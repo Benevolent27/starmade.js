9e58bdd3f76bcf0704bf54960c69621298af38df
Benevolent27 <nolimit8787@yahoo.com>
2020-01-30 05:18:39 -0500

    updated to use installObj

--- mods-ServerDefaults/exampleMods/spawns.js
+++ mods-ServerDefaults/exampleMods/spawns.js
@@ -1,12 +1,12 @@
 
 // I highly recommend reading this article to understand how to do error handling with promises:
 // https://javascript.info/promise-error-handling
-var server=global.getServer(__dirname);
-if (server !== null){
-    server.event.on("playerSpawn", playerSpawn);
-}
+
+var installObj=global.getInstallObj(__dirname); // Get the install object
+var {event}=installObj;
+event.on("playerSpawn", playerSpawn); // Should trigger whenever a player spots
 function playerSpawn (player) {
-    // These will complete as quickly as possible.
+    // These will complete as quickly as possible.  Any connection errors will just display on the console.
     player.botMsg("Welcome to the server you bodacious turtle!").catch((err) => console.error(err));
     player.botMsg("Welcome welcome!",{"type":"info"}).catch((err) => console.error(err));
 };
