e569b6aa1b4618618dc50b94dbf19be235d2f683
Benevolent27 <nolimit8787@yahoo.com>
2020-03-27 00:52:07 -0400

    edited comments

--- mods-ServerDefaults/extensions/objectExtender.js
+++ mods-ServerDefaults/extensions/objectExtender.js
@@ -1,5 +1,5 @@
 // The purpose of this mod is to extend existing objects with more methods.
-// The extensions are only available after the start event, such as when playerMessage, playerDeath, or other events start triggering.
+// The extensions are only available after the start event has finished. This includes when playerMessage, playerDeath, or other server events might start triggering.
 
 // Pull the needed objects from the global variable.
 var {objectHelper}=global;
@@ -14,7 +14,7 @@ event.on('start',function(serverObj){
   SectorObj=serverObj.objects.SectorObj;
   CoordsObj=serverObj.objects.CoordsObj
   SectorObj.prototype.distanceToSector=distanceToSector; // Add a new prototype to the SectorObj
-  serverObj.distanceBetweenSectors=distanceBetweenSectors; // Add a helper to this server
+  serverObj.distanceBetweenSectors=distanceBetweenSectors; // Add a helper to the serverObj
 });
 function distanceToSector(sector,options,cb){
     var objectThis=getOption(options,"this",this); // Contrary to ESLint, this does actually work.  This gets the object's root context or pulls it from the options.
@@ -44,7 +44,10 @@ function distanceBetweenSectors(sector1,sector2){
 function simplePythagorean(a,b){ // Returns c.  "Based on A squared + B squared = C Squared" forumla
     return Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
 }
-function coordinateDifferences(array1,array2){ // Expects arrays with 3 numbers.  This gets the distances on each axis from each point, essentially drawing a triangle and getting the lengths of each side.
+function coordinateDifferences(array1,array2){ 
+  // Expects arrays with 3 numbers each.  
+  // This gets the distances on each axis from each point, essentially drawing a triangle and getting the lengths of each side.
+  // For example, if given [2,3,4] and [10,10,10], this will return [8,7,6]
     var outputArray=[];
     for (let i=0;i<array1.length;i++){
         if (array1[i]>array2[i]){
