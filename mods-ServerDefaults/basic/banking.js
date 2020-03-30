ef147b342db6d57895d4a1db41076924927f1164
Benevolent27 <nolimit8787@yahoo.com>
2020-03-30 07:46:03 -0400

    Added commas to numbers in messages

--- mods-ServerDefaults/basic/banking.js
+++ mods-ServerDefaults/basic/banking.js
@@ -12,12 +12,12 @@ const path=require('path');
 const objectHelper=global.objectHelper;
 var {toNumIfPossible}=objectHelper;
 const miscHelpers=global.miscHelpers;
-var {writeJSONFileSync,getJSONFile,getJSONFileSync,existsAndIsFile,i}=miscHelpers;
+var {writeJSONFileSync,getJSONFile,getJSONFileSync,existsAndIsFile,i,toNumberWithCommas}=miscHelpers;
 
 const installObj=global.getInstallObj(__dirname); // Get the install object
 const installPath=installObj.path;
 const bankingFilePath=path.join(installPath,"banking.json");
-const maxCreditsAstronautCanHold=2147483647;
+const maxCreditsAstronautCanHold=2147483647; // This is a java limitation in StarMade
 const {event,log}=installObj;
 const thisConsole=installObj.console;
 var serverObj={};
@@ -51,14 +51,42 @@ event.on("commandStart",function(regCommand){
   // regCommand("banktransfer", "Banking", false, true,{},banktransfer);
 });
 
-function balance(player, command, args, messageObj){
+async function balance(player, command, args, messageObj){
   if (!bankingFileObj.hasOwnProperty(player.name)){ // If the player has no bank account, set it to 0.
     bankingFileObj[player.name]=0;  
   }
-  return player.botMsg(`Your bank account currently contains ${bankingFileObj[player.name]} credits.`,{fast:true}).catch((err) => console.error(err));
+  var playerCredits=await player.credits().catch((err) => console.error(err));
+  if (typeof playerCredits == "number"){
+    return player.botMsg(`Your bank account currently contains ${bankingFileObj[player.name]} credits and your player inventory has ${playerCredits} credits.`,{fast:true}).catch((err) => console.error(err));
+  }
+  return player.botMsg(`Error retreiving your inventory credit amount!  Please try again!`,{fast:true}).catch((err) => console.error(err));
 }
 
-
+function convertNumShortcuts(inputNumber){ // this is for k,m, and b. Only ensures output is a number if no multiplier specified.
+  var outputNumber=toNumIfPossible(inputNumber); // Removes commas
+  if (typeof outputNumber != "number"){
+    var multiplier=1;
+    if (inputNumber.length > 0){ // Check if there is a multiplier
+      let lastCharNum=inputNumber.length - 1;
+      if (i(inputNumber[lastCharNum],"k")){
+        multiplier=1000;
+      } else if (i(inputNumber[lastCharNum],"m")){
+        multiplier=1000000;
+      } else if (i(inputNumber[lastCharNum],"b")){
+        multiplier=1000000000;
+      }
+      if (multiplier > 1){
+        let tempArray=inputNumber.split("");
+        tempArray.pop();
+        var tempNumber=toNumIfPossible(tempArray.join(""));
+        if (typeof tempNumber=="number"){
+          outputNumber=tempNumber*multiplier;
+        }
+      }
+    }
+  }
+  return toNumIfPossible(outputNumber); // If the value cannot be converted to a number, the input is output untouched
+}
 async function withdraw(player, command, args, messageObj){
   // Expect
   thisConsole.log("Withdraw command ran!");
@@ -66,33 +94,37 @@ async function withdraw(player, command, args, messageObj){
     if (i(args[0],"help")){
       return withdrawHelp(player,{fast:true});
     }
-    var creditsToWithdraw=toNumIfPossible(args[0]);
+    var creditsToWithdraw=args[0];
+    if (!bankingFileObj.hasOwnProperty(player.name)){ // If the player has no bank account, set it to 0.
+      bankingFileObj[player.name]=0;  
+    }
+    if (i(creditsToWithdraw,"all")){
+      creditsToWithdraw=Number(bankingFileObj[player.name]);
+    }
+    creditsToWithdraw=convertNumShortcuts(creditsToWithdraw); // this is for k,m, and b.  Does nothing if no multiplier specified.
     if (typeof creditsToWithdraw == "number"){
       if (creditsToWithdraw > 0){
         var playerCredits=await player.credits().catch((err) => console.error(err));
         if (typeof playerCredits == "number"){
-          if (!bankingFileObj.hasOwnProperty(player.name)){ // If the player has no bank account, set it to 0.
-            bankingFileObj[player.name]=0;  
-          }
           if (bankingFileObj[player.name] >= creditsToWithdraw){
             // First add the credits from the player's inventory
             let playerNewCredits=playerCredits+creditsToWithdraw;
             if (playerNewCredits > maxCreditsAstronautCanHold){ // Astronauts can only carry so many credits.  This is a java limitation.
-              return player.botMsg(`ERROR:  Could not add credits to your inventory!  The max credits an astronaut can carry in StarMade is ${maxCreditsAstronautCanHold} credits!`,{fast:true}).catch((err) => console.error(err));  
+              return player.botMsg(`ERROR:  Could not add credits to your inventory!  The max credits an astronaut can carry in StarMade is ${toNumberWithCommas(maxCreditsAstronautCanHold)} credits!`,{fast:true}).catch((err) => console.error(err));  
             }
             let addResult=await player.giveCredits(creditsToWithdraw);
             if (addResult){
               let playerNewCreditCount=playerCredits+creditsToWithdraw;
               bankingFileObj[player.name]-=creditsToWithdraw; // If successful, remove credits player's bank account
               writeBankingFile();
-              log(`BANKING:  Withdrew ${creditsToWithdraw} for player, ${player.name}, bringing their account to ${bankingFileObj[player.name]} credits.`);
-              return player.botMsg(`SUCCESSFULLY withdrew ${creditsToWithdraw} credits!  Your bank account now has ${bankingFileObj[player.name]} credits and your inventory has ${playerNewCreditCount} credits!`,{fast:true}).catch((err) => console.error(err));
+              log(`BANKING:  Withdrew ${toNumberWithCommas(creditsToWithdraw)} for player, ${player.name}, bringing their account to ${toNumberWithCommas(bankingFileObj[player.name])} credits.`);
+              return player.botMsg(`SUCCESSFULLY withdrew ${toNumberWithCommas(creditsToWithdraw)} credits!  Your bank account now has ${toNumberWithCommas(bankingFileObj[player.name])} credits and your inventory has ${playerNewCreditCount} credits!`,{fast:true}).catch((err) => console.error(err));
             } else {
-              log(`BANKING: Failed to add credits to inventory for '${player.name}' during withdraw.  Credits attempted: ${creditsToWithdraw}`);
+              log(`BANKING: Failed to add credits to inventory for '${player.name}' during withdraw.  Credits attempted: ${toNumberWithCommas(creditsToWithdraw)}`);
               return player.botMsg(`ERROR:  Could not add credits to your inventory!  Please try again! (Your bank credits are SAFE!)`,{fast:true}).catch((err) => console.error(err));  
             }
           } else {
-            return player.botMsg(`Sorry, you do not have that many credits in your bank account!  You only have ${bankingFileObj[player.name]} credit(s)!`,{fast:true}).catch((err) => console.error(err));
+            return player.botMsg(`Sorry, you do not have that many credits in your bank account!  You only have ${toNumberWithCommas(bankingFileObj[player.name])} credit(s)!`,{fast:true}).catch((err) => console.error(err));
           }
         } else {
           log(`BANKING: Failed to retreive credit count for '${player.name}' during withdraw.`);
@@ -123,9 +155,6 @@ function withdrawHelp(player,options){
   player.msg(`Example 3: !withdraw 50k`,options).catch((err) => console.error(err));
 }
 
-
-
-
 async function deposit(player, command, args, messageObj){
   // Expect
   thisConsole.log("Deposit command ran!");
@@ -133,11 +162,17 @@ async function deposit(player, command, args, messageObj){
     if (i(args[0],"help")){
       return depositHelp(player,{fast:true});
     }
-    var creditsToDeposit=toNumIfPossible(args[0]);
-    if (typeof creditsToDeposit == "number"){
-      if (creditsToDeposit > 0){
-        var playerCredits=await player.credits().catch((err) => console.error(err));
-        if (typeof playerCredits == "number"){
+    var playerCredits=await player.credits().catch((err) => console.error(err));
+    if (typeof playerCredits == "number"){ 
+      if (playerCredits == 0){
+        return player.botMsg(`Sorry, you do not have any credits in your inventory to deposit!`,{fast:true}).catch((err) => console.error(err));
+      }
+      var creditsToDeposit=convertNumShortcuts(args[0]);
+      if (creditsToDeposit=="all"){
+        creditsToDeposit=playerCredits;
+      }
+      if (typeof creditsToDeposit == "number"){
+        if (creditsToDeposit > 0){
           if (playerCredits >= creditsToDeposit){
             // First withdraw the credits from the player's inventory
             let negativeCreditsToDeposit=creditsToDeposit * -1;
@@ -148,26 +183,26 @@ async function deposit(player, command, args, messageObj){
               }
               bankingFileObj[player.name]+=creditsToDeposit;
               writeBankingFile();
-              log(`BANKING:  Deposited ${creditsToDeposit} for player, ${player.name}, bringing their account to ${bankingFileObj[player.name]} credits.`);
-              return player.botMsg(`SUCCESSFULLY deposited ${creditsToDeposit} credits!  Your bank account now has ${bankingFileObj[player.name]} credits!`,{fast:true}).catch((err) => console.error(err));
+              log(`BANKING:  Deposited ${toNumberWithCommas(creditsToDeposit)} for player, ${player.name}, bringing their account to ${toNumberWithCommas(bankingFileObj[player.name])} credits.`);
+              return player.botMsg(`SUCCESSFULLY deposited ${toNumberWithCommas(creditsToDeposit)} credits!  Your bank account now has ${toNumberWithCommas(bankingFileObj[player.name])} credits!`,{fast:true}).catch((err) => console.error(err));
             } else {
               log(`BANKING: Failed to remove credit count for '${player.name}' during deposit.  Credits attempted: ${creditsToDeposit}`);
               return player.botMsg(`ERROR:  Could not remove credits from your inventory!  Please try again!`,{fast:true}).catch((err) => console.error(err));  
             }
           } else {
-            return player.botMsg(`Sorry, you do not have that many credits in your inventory!  You only have ${playerCredits} credit(s)!`,{fast:true}).catch((err) => console.error(err));
+            return player.botMsg(`Sorry, you do not have that many credits in your inventory!  You only have ${toNumberWithCommas(playerCredits)} credit(s)!`,{fast:true}).catch((err) => console.error(err));
           }
         } else {
-          log(`BANKING: Failed to retreive credit count for '${player.name}' during deposit.`);
-          return player.botMsg("ERROR:  Something went wrong when attempting to deposit your credits.  Your credits are SAFE.  Please try again!",{fast:true}).catch((err) => console.error(err));
+          await player.botMsg("ERROR:  Invalid amount of credits given!  Please provide a POSITIVE number!  If you would like to withdraw credits, please use the WITHDRAW command!",{fast:true}).catch((err) => console.error(err));
+          return depositHelp(player,{fast:true});
         }
       } else {
-        await player.botMsg("ERROR:  Invalid amount of credits given!  Please provide a POSITIVE number!  If you would like to withdraw credits, please use the WITHDRAW command!",{fast:true}).catch((err) => console.error(err));
+        player.botMsg("ERROR:  Invalid amount of credits given!  Please provide a number!",{fast:true}).catch((err) => console.error(err));
         return depositHelp(player,{fast:true});
       }
     } else {
-      player.botMsg("ERROR:  Invalid amount of credits given!  Please provide a number!",{fast:true}).catch((err) => console.error(err));
-      return depositHelp(player,{fast:true});
+      log(`BANKING: Failed to retreive credit count for '${player.name}' during deposit.`);
+      return player.botMsg("ERROR:  Something went wrong when attempting to deposit your credits.  Your credits are SAFE.  Please try again!",{fast:true}).catch((err) => console.error(err));
     }
   } else if (args.length > 1){
     player.botMsg("ERROR:  Too many arguments!",{fast:true}).catch((err) => console.error(err));
