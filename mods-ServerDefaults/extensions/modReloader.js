e360755cec68da2a39b7c2e1372e388b96646f67
Benevolent27 <nolimit8787@yahoo.com>
2020-04-02 04:00:45 -0400

    switching to new format for commands

--- mods-ServerDefaults/extensions/modReloader.js
+++ mods-ServerDefaults/extensions/modReloader.js
@@ -10,31 +10,32 @@ var serverObj={};
 event.on("start",function(theServerObj){
   serverObj=theServerObj;
 });
+
 event.on('commandStart',function(regCommand){
-    regCommand("reloadMods","HiddenHelpers",true,false); // Admin only command
-    regCommand("listeners","HiddenHelpers",true,false);
-    event.on('playerCommand', function (player,command,args,messageObj) { // Normally we would not use the messageObj, but it's here if for some reason we want the command to operate differently depending on channel sent to
-        if (command == "reloadmods"){
-            player.botMsg("Reloading all mods!  This might take a few seconds.",{fast:true}).catch((err) => console.error(err));
-            reloadMods(); // This will emit a "unloadMods" event, which the serverObj is listening for
-            // event.emit("unloadMods");
-            // TODO: I need to send a global command to remove the event listeners, this will not suffice.
-            player.botMsg("Done!",{fast:true}); // This is syncronous
-        } else if (command == "listeners"){ // TODO: Change this so it looks at all event listeners and then lists each.
-          player.botMsg("Displaying event listeners to the wrapper console..",{fast:true}).catch((err) => console.error(err));
-          thisConsole.log("command:");
-          thisConsole.dir(event.listeners('command'));
-          thisConsole.log("playerSpawn:");
-          thisConsole.dir(event.listeners('playerSpawn'));
-          thisConsole.log("shipSpawn:");
-          thisConsole.dir(event.listeners('shipSpawn'));
-          thisConsole.log("baseSpawn:");
-          thisConsole.dir(event.listeners('baseSpawn'));
-          thisConsole.log("blueprintSpawn:");
-          thisConsole.dir(event.listeners('blueprintSpawn'));
-          thisConsole.log("reloadMods:");
-          thisConsole.dir(event.listeners('reloadMods'));
-        }
-    });
+    regCommand("reloadMods","HiddenHelpers",true,false,{},reloadModsCommand); // Admin only command
+    regCommand("listeners","HiddenHelpers",true,false,{},listeners);
 });
 
+function listeners (player,command,args,messageObj) { // Normally we would not use the messageObj, but it's here if for some reason we want the command to operate differently depending on channel sent to
+  player.botMsg("Displaying event listeners to the wrapper console..",{fast:true}).catch((err) => console.error(err));
+  thisConsole.log("command:");
+  thisConsole.dir(event.listeners('command'));
+  thisConsole.log("playerSpawn:");
+  thisConsole.dir(event.listeners('playerSpawn'));
+  thisConsole.log("shipSpawn:");
+  thisConsole.dir(event.listeners('shipSpawn'));
+  thisConsole.log("baseSpawn:");
+  thisConsole.dir(event.listeners('baseSpawn'));
+  thisConsole.log("blueprintSpawn:");
+  thisConsole.dir(event.listeners('blueprintSpawn'));
+  thisConsole.log("reloadMods:");
+  thisConsole.dir(event.listeners('reloadMods'));
+}
+
+function reloadModsCommand(player,command,args,messageObj){
+  player.botMsg("Reloading all mods!  This might take a few seconds.",{fast:true}).catch((err) => console.error(err));
+  reloadMods(); // This will emit a "unloadMods" event, which the serverObj is listening for
+  // event.emit("unloadMods");
+  // TODO: I need to send a global command to remove the event listeners, this will not suffice.
+  player.botMsg("Done!",{fast:true}); // This is syncronous
+}
